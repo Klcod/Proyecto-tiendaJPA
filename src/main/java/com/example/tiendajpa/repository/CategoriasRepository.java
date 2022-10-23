33d292ad50e87e77e69e764ad80e418d6e0af501
Angel Canche <angel14poot@gmail.com>
2022-10-23 15:05:30 -0500

    Leccion 8 videos 1-22
    se crean todos los métodos

---
+++ src/main/java/com/example/tiendajpa/repository/CategoriasRepository.java
@@ -0,0 +1,9 @@
+package com.example.tiendajpa.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.tiendajpa.model.Categoria;
+
+public interface CategoriasRepository extends CrudRepository<Categoria, Integer> {
+
+}
